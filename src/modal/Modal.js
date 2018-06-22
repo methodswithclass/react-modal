764a1ef4465ecda927de44db462f9d61579e27a4
Christopher Polito <cpolito343@gmail.com>
2018-06-22 04:59:54 -0700

    refactor and additional features
    refactor to fix bugs. Modal has become single component including open button and single state, added feature of variable message

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Modal.css';
 
-import ModalBody from "./ModalBody"
+
 
 class Modal extends Component {
 
@@ -12,25 +12,58 @@ class Modal extends Component {
 
 
     this.state = {
-      open:false
+      open:false,
+      count:1
     }
+
+    this.handleClick = this.handleClick.bind(this);
+
   }
+
 
   handleClick (e) {
 
-      this.setState({open:true})
+    this.setState({
+      open:!this.state.open,
+      count:this.state.count+(this.state.open ? 1 : 0)
+    });
   }
 
 
-
   render() {
+
+    var modalStyle = {
+        position:"absolute",
+        width:"100%",
+        height:"100%",
+        visibility:(this.state.open ? 'visible' : 'hidden')
+    }
+
+
     return (
-      <div className="Button">
+      <div className="Modal">
         <div className="btn center" onClick={this.handleClick}>
           <div className="center">open</div>
         </div>
 
-        <ModalBody open={this.state.open}/>
+
+        <div style={modalStyle}>
+          <div className="modal-background" onClick={this.handleClick}></div>
+          <div className="modal-inner center">
+            
+            <div className="modal-container center">
+              <div className="modal-title hcenter">
+                <div className="center">{this.props.message}{this.state.count}</div>
+              </div>
+              
+              <div className="modal-button btn hcenter" onClick={this.handleClick}>
+                <div className="center">close</div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
