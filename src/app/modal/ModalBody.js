3818edf698fb1ac53e0abfef5ba2a29fa9695990
Christopher Polito <chris@mwclass.io>
2022-06-04 17:48:25 -0400

    convert to functional components

--- src/app/modal/ModalBody.js
+++ src/app/modal/ModalBody.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ModalBody extends Component {
-  render() {
-    const { handleClick, open, message, count } = this.props;
+const ModalBody = (props) => {
+  const { handleClick, open, message, count } = props;
 
-    return (
-      <>
-        {open ? (
-          <>
-            <div className="modal-background" onClick={handleClick}></div>
-            <div className="modal-inner center">
-              <div className="modal-container center">
-                <div className="modal-title hcenter">
-                  <div className="center">{`${message}${count}`}</div>
-                </div>
-
-                <button
-                  className="modal-button btn hcenter"
-                  onClick={handleClick}
-                >
-                  close
-                </button>
+  return (
+    <>
+      {open ? (
+        <>
+          <div className="modal-background" onClick={handleClick}></div>
+          <div className="modal-inner center">
+            <div className="modal-container center">
+              <div className="modal-title hcenter">
+                <div className="center">{`${message}${count}`}</div>
               </div>
+
+              <button
+                className="modal-button btn hcenter"
+                onClick={handleClick}
+              >
+                close
+              </button>
             </div>
-          </>
-        ) : null}
-      </>
-    );
-  }
-}
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
 
 export default ModalBody;
--- src/app/modal/__tests__/ModalBody.test.js
+++ src/app/modal/__tests__/ModalBody.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ModalBody from './ModalBody';
+import ModalBody from '../ModalBody';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
